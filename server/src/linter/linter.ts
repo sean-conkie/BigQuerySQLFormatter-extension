0cd1a55778ab3d3eed0c18ac0f426cdf43059dc0
sean-conkie <sean.conkie@sky.uk>
2024-03-04 09:54:30 +0000

    Add SQL files to .gitignore and refactor Linter class

--- server/src/linter/linter.ts
+++ server/src/linter/linter.ts
@@ -9,16 +9,13 @@
 import { ServerSettings } from '../settings';
 
 import {
-	Diagnostic,
-	DiagnosticSeverity
+	Diagnostic, integer,
 } from 'vscode-languageserver/node';
+import { Rule } from './rules/base';
+import { initialiseRules } from './rules/rules';
+import { RuleType } from './rules/enums';
 
 
-/**
- * The map to store private data.
- * @type {WeakMap<Linter, LinterInternalSlots>}
- */
-const internalSlotsMap = new WeakMap();
 
 /**
  * Object for linting SQL code
@@ -26,26 +23,51 @@ const internalSlotsMap = new WeakMap();
  */
 export class Linter {
 
+	settings: ServerSettings;
+	regexRules: Rule[] = [];
+	parserRules: Rule[] = [];
+	problems: integer = 0;
+
 	constructor(settings: ServerSettings) {
-		internalSlotsMap.set(this, {
-		});
+		this.settings = settings;
+		this._initialiseRules();
+	}
+
+	/**
+	 * Initialise the rules
+	 * @memberof Linter
+	 * @name initialiseRules
+	 */
+	_initialiseRules() {
+		const rules: Rule[] = initialiseRules(this.settings, this.problems);
+
+		this.regexRules = rules.filter(rule => rule.type === RuleType.REGEX);
+		this.parserRules = rules.filter(rule => rule.type === RuleType.PARSER);
+
 	}
 
 	/**
 	 * Lint the source code
 	 * @param {string} source The source code to lint
-	 * @param {object} [config={}] The configuration to use
 	 * @returns {Diagnostic[]} The diagnostics found in the source code
 	 * @memberof Linter
 	 */
-	verify(source: string, config: object = {}): Diagnostic[] {
-		const internalSlots = internalSlotsMap.get(this);
+	verify(source: string): Diagnostic[] {
 
 		const diagnostics: Diagnostic[] = [];
 
+		for (const rule of this.regexRules) {
+			const result = rule.evalute(source);
+			if (result !== null) {
+				diagnostics.push(result);
+				this.problems++;
+			}
+		}
 
 		return diagnostics;
 		
 	}
+
+
 
 }
