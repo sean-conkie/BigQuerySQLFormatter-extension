b0279466ad5deb860f03925f15b3c41cc3f74c04
sean-conkie <sean.conkie@sky.uk>
2024-03-06 15:11:59 +0000

    Add @types/oniguruma and oniguruma dependencies

--- server/src/linter/rules/layout/rules.ts
+++ server/src/linter/rules/layout/rules.ts
@@ -6,12 +6,14 @@
  * @requires Rule
  */
 
-import { StartOfFile } from './LT13';
 import { Rule } from '../base';
 import { ServerSettings } from '../../../settings';
+import { EndofFile } from './LT12';
+import { StartOfFile } from './LT13';
 
 export function layoutRules(settings: ServerSettings, problems: number): Rule[] {
 	return [
-		new StartOfFile(settings, problems)
+		new StartOfFile(settings, problems),
+		new EndofFile(settings, problems)
 	];
 }
