c1ee13a7f1d2f3d008d26b673b50df5cfc69419e
aad86 <aamna.ahmed@sky.uk>
2024-03-13 13:22:34 +0000

    Adding changes to rules.ts and rules.test.ts

--- server/src/linter/rules/layout/rules.ts
+++ server/src/linter/rules/layout/rules.ts
@@ -6,12 +6,14 @@
  * @requires Rule
  */
 
-import { StartOfFile } from './LT13';
 import { Rule } from '../base';
 import { ServerSettings } from '../../../settings';
+import { UnionCheck } from './LT11';
+import { StartOfFile } from './LT13';
 
 export function layoutRules(settings: ServerSettings, problems: number): Rule[] {
 	return [
+		new UnionCheck(settings, problems),
 		new StartOfFile(settings, problems)
 	];
 }
