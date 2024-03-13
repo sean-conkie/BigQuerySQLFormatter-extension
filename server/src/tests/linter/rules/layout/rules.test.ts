f304de0942e3c16c08a0d51b89e047d3c05a4cf0
Aamna Ahmed <116823520+aamna-ahmed@users.noreply.github.com>
2024-03-13 13:40:39 +0000

    Add Rule: UNION operators should be surrounded by newlines (#77)

--- server/src/tests/linter/rules/layout/rules.test.ts
+++ server/src/tests/linter/rules/layout/rules.test.ts
@@ -5,21 +5,25 @@
 
 import { expect } from 'chai';
 import { layoutRules } from '../../../../linter/rules/layout/rules';
+import { UnionCheck } from '../../../../linter/rules/layout/LT11';
 import { StartOfFile } from '../../../../linter/rules/layout/LT13';
 import { defaultSettings } from '../../../../settings';
 
 describe('layoutRules', () => {
-    it('should return an array with a single instance of StartOfFile', () => {
+    it('should return an array with a single instance of each rule', () => {
         const problems = 0;
         const result = layoutRules(defaultSettings, problems);
-        expect(result).to.be.an('array').that.has.lengthOf(1);
-        expect(result[0]).to.be.an.instanceOf(StartOfFile);
+        expect(result).to.be.an('array').that.has.lengthOf(2);
+        expect(result[0]).to.be.an.instanceOf(UnionCheck);
+        expect(result[1]).to.be.an.instanceOf(StartOfFile);
     });
 
-    it('should pass the settings and problems to the StartOfFile constructor', () => {
+    it('should pass the settings and problems to each constructor', () => {
         const problems = 5;
         const result = layoutRules(defaultSettings, problems);
         expect(result[0].settings).to.equal(defaultSettings);
         expect(result[0].problems).to.equal(problems);
+        expect(result[1].settings).to.equal(defaultSettings);
+        expect(result[1].problems).to.equal(problems);
     });
 });
