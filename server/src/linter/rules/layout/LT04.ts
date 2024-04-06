2a8b7ed6661acb96965757397338ef71434679d4
sean-conkie <sean.conkie@sky.uk>
2024-04-06 19:57:37 +0000

    Add TrailingComma rule to enforce trailing comma checks

---
+++ server/src/linter/rules/layout/LT04.ts
@@ -0,0 +1,73 @@
+/**
+ * @fileoverview Rules to enforce trailing comma checks
+ * @module linter/rules/layout
+ * @requires vscode-languageserver
+ * @requires settings
+ * @requires Rule
+ */
+
+
+import { ServerSettings } from "../../../settings";
+import { RuleType } from '../enums';
+import {
+  Diagnostic,
+} from 'vscode-languageserver/node';
+import { Rule } from '../base';
+import { StatementAST } from '../../parser';
+
+
+/**
+ * The TrailingComma rule
+ * @class TrailingComma
+ * @extends Rule
+ * @memberof Linter.Rules
+ */
+export class TrailingComma extends Rule{
+  readonly name: string = "trailing_commas";
+  readonly code: string = "LT04";
+	readonly type: RuleType = RuleType.PARSER;
+  readonly message: string = "Commas should be at the end of the line.";
+
+  /**
+   * Creates an instance of TrailingComma.
+   * @param {ServerSettings} settings The server settings
+   * @param {number} problems The number of problems identified in the source code
+   * @memberof TrailingComma
+   */
+  constructor(settings: ServerSettings, problems: number) {
+      super(settings, problems);
+  }
+
+  evaluate(test: string): Diagnostic[] | null {
+
+    return null;
+
+  }
+
+  evaluateAst(ast: {[key: number] : StatementAST}): Diagnostic[] | null {
+
+    if (this.enabled === false) {
+      return null;
+    }
+
+		const errors: Diagnostic[] = [];
+    for (const i in ast) {
+      for (const column of ast[i].columns) {
+        const filteredTokens = column.tokens.filter((token) => !token.scopes.includes("punctuation.whitespace.leading.sql"));
+        if ((filteredTokens[0].value??'') === ',') {
+          errors.push({
+            message: this.message,
+            severity: this.severity,
+            range: {
+              start: { line: filteredTokens[0].line??0, character: filteredTokens[0].start??0 },
+              end: { line: filteredTokens[0].line??0, character: filteredTokens[0].end??0 }
+            },
+            source: this.name
+          });
+        }
+      }
+    }
+		
+    return errors;
+  }
+}
