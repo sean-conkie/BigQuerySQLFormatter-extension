56bedf4ddc29e2958dc0c68967bb8912daad2bcd
sean-conkie <seanpconkie@yahoo.com>
2024-05-16 19:51:29 +0000

    refactor: Add getLineAndCharacter method to Rule base class

--- server/src/linter/rules/base.ts
+++ server/src/linter/rules/base.ts
@@ -13,6 +13,8 @@ import { OnigRegExp } from 'oniguruma';
 import { FileMap } from '../parser';
 
 
+export type MatchPosition = { line: number, character: number }
+
 /**
  * Abstract class for a rule
  * @class Rule
@@ -44,5 +46,17 @@ export abstract class Rule<T extends string | FileMap>{
 	}
 
 	abstract evaluate(test: T): Diagnostic[] | null;
+
+	getLineAndCharacter(content: string, matchIndex: number):  MatchPosition{
+			const lines = content.split('\n');
+			let runningTotal = 0;
+			for (let i = 0; i < lines.length; i++) {
+					if (runningTotal + lines[i].length + 1 > matchIndex) {
+							return { line: i, character: matchIndex - runningTotal };
+					}
+					runningTotal += lines[i].length + 1;
+			}
+			throw new Error('Match index out of range');
+	}
 
 }
