295772ff4348c4f0a40763fb309eef9ef6a7c267
sean-conkie <seanpconkie@yahoo.com>
2024-05-16 21:31:27 +0000

    refactor: Remove e2e.sh script for running tests

--- scripts/e2e.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CODE_TESTS_PATH="$(pwd)/client/out/test"
-CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
-export CODE_TESTS_PATH CODE_TESTS_WORKSPACE
-
-node "$(pwd)/client/out/test/runTest"
