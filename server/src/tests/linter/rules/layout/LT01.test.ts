1dcaa5e8a0d3ab7ac30ca5e883f09f476a0fe43a
sean-conkie <seanpconkie@yahoo.com>
2024-05-16 20:52:50 +0000

    refactor: Add LT01.test.ts for TrailingSpaces rule

---
+++ server/src/tests/linter/rules/layout/LT01.test.ts
@@ -0,0 +1,108 @@
+/**
+ * @fileoverview Test suite for LT11 module
+ */
+
+import { expect } from 'chai';
+import { defaultSettings } from '../../../../settings';
+import { TrailingSpaces } from '../../../../linter/rules/layout/LT01';
+import { runInThisContext } from 'vm';
+
+describe('TrailingSpaces', () => {
+    let instance: TrailingSpaces;
+
+    beforeEach(() => {
+        instance = new TrailingSpaces(defaultSettings, 0);
+    });
+
+    it('Case 1: should return null when rule is disabled', () => {
+        instance.enabled = false;
+        const result = instance.evaluate('test');
+        expect(result).to.be.null;
+    });
+
+    const listSql: string[][] = [
+        ['Case 2: trailing spaces',
+        `select * \n
+        from tablename`,'0','8',
+    '0','9'],
+ 
+    ];
+
+    listSql.forEach((element: string[]) => {
+        it(`${element[0]}\nshould return diagnostic when rule is enabled and pattern matches`, () => {
+            instance.enabled = true;
+            const result = instance.evaluate(element[1]);
+            expect(result).to.deep.equal([{
+                message: instance.message,
+                severity: instance.severity,
+                range: {
+                    start: { line: parseInt(element[2]), character: parseInt(element[3]) },
+                    end: { line: parseInt(element[4]), character: parseInt(element[5]) }
+                },
+                source: 'LT01 (trailing_sapces)'
+            }]);
+        });
+    });
+
+    const listSQLMultipleErrors: string[][] = [
+
+
+        ['Case 3: multiple errors',
+        `select * \n
+         from tablename\nselect
+         * \nfrom tablename`,'0','8','0','9','4','10','4','11'],];
+
+
+
+          listSQLMultipleErrors.forEach((element: string[]) => {
+            it(`${element[0]}\nshould return diagnostic when rule is enabled and pattern matches`, () => {
+                instance.enabled = true;
+                const result = instance.evaluate(element[1]);
+                expect(result).to.deep.equal([{
+                    message: instance.message,
+                    severity: instance.severity,
+                    range: {
+                        start: { line: parseInt(element[2]), character: parseInt(element[3]) },
+                        end: { line: parseInt(element[4]), character: parseInt(element[5]) }
+                    },
+                    source: 'LT01 (trailing_sapces)'
+                },{
+                    message: instance.message,
+                    severity: instance.severity,
+                    range: {
+                        start: { line: parseInt(element[6]), character: parseInt(element[7]) },
+                        end: { line: parseInt(element[8]), character: parseInt(element[9]) }
+                    },
+                    source: 'LT01 (trailing_sapces)'
+                }]);
+            });
+        });
+
+    const listSqlMultiples: string[][] = [
+        ['Case 4: multiple errors',
+        `select * \n
+        from tablename\nselect
+        * \nfrom tablename`],
+    ];
+
+    listSqlMultiples.forEach((element: string[]) => {
+        it(`${element[0]}\nshould return number of matches greater than 1 when rule is enabled and pattern matches more than once`, () => {
+            instance.enabled = true;
+            const noOfMatches = instance.matches(element[1]);
+            expect(noOfMatches).to.be.greaterThan(1);
+        });
+    });
+
+    const listSqlNoMatch: string[][] = [
+        ['Case 5: pattern does not match (union all)',
+        `select *\nfrom table`],
+    ];
+
+    listSqlNoMatch.forEach((element: string[]) => {
+        it(`${element[0]}\nshould return null when rule is enabled but pattern does not match`, () => {
+            instance.enabled = true;
+            const result = instance.evaluate(element[1]);
+            expect(result).to.be.null;
+        });
+    });
+});
