b91b022de35a12f08b350f3322607c288c4aac74
sean-conkie <sean.conkie@sky.uk>
2024-10-11 13:40:38 +0100

    docs: Update README and Sphinx configuration to include version from package.json

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,23 @@
 # Configuration file for the Sphinx documentation builder.
-
+import json
+from pathlib import Path
 # -- Project information
 
 project = "BigQuery SQL Formatter"
 copyright = "2024, sean-conkie"
 author = "sean-conkie"
 
+
 release = "0.1"
 version = "0.0.5"
+
+# find the package.json at the project root
+package_json = Path(__file__).parents[2] / "package.json"
+if package_json.exists():
+    with package_json.open() as f:
+        package = json.load(f)
+        release = package["version"]
+        version = release.split("-")[0]
 
 # -- General configuration
 
