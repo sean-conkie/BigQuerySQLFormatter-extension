3a92bcf7b8c003ff904c4e088c0a053065a889d7
sean-conkie <sean.conkie@sky.uk>
2024-10-11 19:49:29 +0000

    test: Add tests for IsNull rule diagnostics and code actions

--- server/src/tests/linter/rules/convention/CV05.test.ts
+++ server/src/tests/linter/rules/convention/CV05.test.ts
@@ -63,6 +63,49 @@ describe('IsNull', () => {
         }));
     });
 
+    it('should return diagnostic when rule is enabled and pattern matches', () => {
+        instance.enabled = true;
+        const result = instance.evaluate('select a.col != null from dataset.table a left join dataset.table b on a.col = b.col');
+        expect(result).to.deep.equal([{
+            code: instance.diagnosticCode,
+            codeDescription: {href: instance.diagnosticCodeDescription},
+            message: instance.message,
+            severity: instance.severity,
+            range: {
+                start: { line: 0, character: 13 },
+                end: { line: 0, character: 15 }
+            },
+            source: instance.source
+        }]);
+    });
+
+    it('should return codeaction when rule is enabled and as used', async () => {
+        instance.enabled = true;
+        const parser = new Parser();
+        await parser.parse({text:'select a.col != null from dataset.table a left join dataset.table b on a.col = b.col', uri: 'test.sql', languageId: 'sql', version: 0});
+        const diagnostics = instance.evaluate('select a.col != null from dataset.table a left join dataset.table b on a.col = b.col');
+        const actions = instance.createCodeAction({uri: 'test.sql'}, diagnostics![0]);
+        expect(actions).to.deep.equal(instance.codeActionKind.map(kind => {
+            return {
+                title: 'Replace with is null', edit:{
+                changes: {
+                        ['test.sql']: [
+                            {
+                                newText: 'is null',
+                                range: {
+                                    start: { line: 0, character: 13 },
+                                    end: { line: 0, character: 15 }
+                                }
+                            }
+                        ]
+                    }
+                },
+                kind: kind,
+                diagnostics: diagnostics
+            };
+        }));
+    });
+
     it('should return null when rule is enabled but pattern does not match', () => {
         instance.enabled = true;
         const result = instance.evaluate('select a.col is null from dataset.table a left join dataset.table b on a.col = b.col');
